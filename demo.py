6214b633e8ca4bf04d5014aca6581e784ec869ff
zincsabian <2773467721@qq.com>
2025-02-11 21:57:42 +0800

    update demo

--- demo.py
+++ demo.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 searcher = WebSearcher()
-query = "Python编程"
+query = "fix memory leak in cpp"
 scraped = searcher.search_and_scrape(query)
 for url, content in scraped.items():
     print(f"URL: {url}:\ncontent: {content[:500]}\n")
